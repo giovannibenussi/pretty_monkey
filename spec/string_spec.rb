87b765933dc08c76a890021a397725fb2defe52c
Giovanni Benussi <giovanni.benussi@usach.cl>
2017-02-18 18:01:21 -0300

    Add test for String#remove_consonants

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -3,4 +3,9 @@ RSpec.describe String do
         text = 'hello world'
         expect(text.remove_consonants).to eq('eoo')
     end
+    it "remove the consonants of the string (bang version)" do
+        text = 'hello world'
+        text.remove_consonants!
+        expect(text).to eq('eoo')
+    end
 end
